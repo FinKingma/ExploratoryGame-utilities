9f91723038ae4dc0f092d658c1356cf109327ac7
Fin Kingma <Kingma@zorgdomein.nl>
2017-02-21 14:02:56 +0100

    opgeschoond

--- MapMakerApi2/public/javascripts/timebox.js
+++
@@ -1,7 +0,0 @@
-function initialize() {
-    var timebox = 10000;
-    var timer = setInterval(function() {
-        timebox--;
-        console.log(timebox);
-    },1000);
-}
--- MapMakerApi2/routes/game.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.post('/', function(req, res, next) {
-  res.render('game', { controls: req.body.controls });
-});
-
-module.exports = router;
